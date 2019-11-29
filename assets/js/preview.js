471313ce50e5eca2e51ee74737cc6002c2eb2bb8
noelelias <schenknoel@gmail.com>
2019-11-29 17:30:03 +0100

    page titles

--- assets/js/preview.js
+++ assets/js/preview.js
@@ -1,8 +1,23 @@
 $(function() {
     isMobile = $(window).width() < 992;
-    $('main .pages .map').click((event)=>{
-        if(isMobile){
-            $(event.currentTarget).toggleClass('choose');
-        }
-    });
+
+    init();
+    function init(){
+        page_change();
+        events();
+    }
+    function events(){
+        $('main .pages .map').click((event)=>{
+            if(isMobile){
+                $(event.currentTarget).toggleClass('choose');
+            }
+        });
+        $('main nav a').click(()=>{
+            page_change();
+        });
+    }
+    function page_change(){
+        var current_page_title = $('.pages > div.active > *').attr('page-title')
+        $('header h1 small').html(current_page_title);
+    }
 });
